d005bf0b9c1357fdf75dc08f7b48b4648d17436d
thabangkgatlhanye7117 <thabangkgatlhanye7117@gmail.com>
2025-08-10 11:21:14 +0200

    seo meta tags

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
+import Layout from './Layout.jsx'
+
+const router = createBrowserRouter([
+{
+  path: "/", element: "Layout",
+  children : [
+    {/*index: true , element: ,<HomePage/> */},
+  ]
+
+}
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </StrictMode>,
 )
