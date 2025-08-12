103981211234092e6bd92418ced0af6295fb5402
thabangkgatlhanye7117 <thabangkgatlhanye7117@gmail.com>
2025-08-12 22:22:05 +0200

    about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,40 @@
 const AboutPage = () => {
   return(
     <section className="about-section">
+      <div className="about-info">
       <h3 className="about-title">
         About us
       </h3>
+      <p>
+            <b>Gully Closet</b> was established in 2016,<br/>
+            as a reselling and styling company by<br/>
+            Lethabo Pilane.
+            <br/>
+            <br/>
+            Specializing in brands in brands like Bape, Carhartt<br/>
+            Daily x Paper, Nike, Vetements, Stussy and many more...
+      </p>
+      <p>
+            <b>Why</b><br/>
+            Inspired by a Documentary called "The True Cost",that hightlights 
+            the effects of fast fashion on the planet and labor conditions of the 
+            people working behind the scenes in sweat shops.<br/>
+
+            I buy for reselling, styling & deconstruction purposes making quality brands 
+            accessible to everyone at a reasonable price tag
+      </p>
+      <p>
+            <b>What I do</b><br/>
+            I offer quality services, reselling, consigning, styling & designing for almost 
+            every kind of style from different eras and times but keep the feel of the look
+            modern with the clients own taste and touch for any occasion or season.<br/><br/>
+            Create a new market for upcoming local &
+            international brands by using their products
+            in different projects...
+      </p>
+      </div>
+
+      
 
     </section>
   )
