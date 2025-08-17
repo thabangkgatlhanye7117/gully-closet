8248e4e9de49bd92aa30cd826c6ed409a6943850
thabangkgatlhanye7117 <thabangkgatlhanye7117@gmail.com>
2025-08-17 22:38:51 +0200

    offerings section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -16,10 +16,25 @@ const HomePage = () => {
                     <h3 className="new-arrivals-title">
                         New Arrivals
                     </h3>
-                   { /*maping through new arrives two rows */}
+                   {/*maping through new arrives two rows */}
                 </section>
                 <section className="business-offerings-section">
                     <div className="business-offerings">
+                        <div className="offering">
+                            <i className="ri-checkbox-circle-line"></i>
+                            <h3>SELLING AUTHENTIC THRIFT APPAREL</h3>
+                            <span>Premium apparel, verified for authenticity and origin</span>
+                        </div>
+                           <div className="offering">
+                            <i className="ri-truck-line"></i>
+                            <h3>NATIONWIDE SHIPPING</h3>
+                            <span>We deliver anywhere in South Africa fast, reliable and secure</span>
+                        </div>
+                           <div className="offering">
+                            <i className="ri-phone-line"></i>
+                            <h3>OUR SERVICES</h3>
+                            <span>Selling, consigning, styling and designing</span>
+                        </div>
 
                     </div>
                     <div className="tagline">
