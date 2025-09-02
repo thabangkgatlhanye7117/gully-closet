4c7c730489763ba979f49ea3b333919a08ab6263
thabangkgatlhanye7117 <thabangkgatlhanye7117@gmail.com>
2025-09-02 21:10:47 +0200

    modified

--- src/pages/ProductViewPage.jsx
+++ src/pages/ProductViewPage.jsx
@@ -9,15 +9,16 @@ const ProductViewPage = () => {
   return (
     <div className="product-view-page">
        <div className="img-slider">
-        
+          <img src={state.selectedProduct.image} alt="selected product image" className="slider-img" loading='lazy' />
        </div>
        <div className="viewed-product-info">
+           <div className="viewed-product-category">{state.selectedProduct.category}</div>
            <div className="viewed-product-name">{state.selectedProduct.name}</div>
            <div className="viewed-product-price">R{state.selectedProduct.price}</div>
            <div className= "viewed-product-size">{state.selectedProduct.size}</div>
            <div className="viewed-product-condition">{state.selectedProduct.condition}</div>
            <div className="viewed-product-buttons">
-              <button onclick={()=> dispatch({type:"Add", product:selectedProduct})}>Add to cart</button>
+              <button onClick={()=> dispatch({type:"Add", product:selectedProduct})}>ADD TO CART</button>
            </div>
 
        </div>
